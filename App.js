3b2f4e341d39dff6836d16e0954128b532d86e0c
jeremyMasse <jeremy954702305@gmail.com>
2023-03-30 13:47:52 +0200

    reset projet à 0

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import Routes from './src/routes';
-import {FavoritesProvider} from './src/contexts/FavoritesContext';
-import {ToasterProvider} from './src/contexts/ToasterContext';
-import Toaster from './src/components/Toaster';
-// import { Provider } from 'react-redux';
 
 /**
  * Point de départ de l'application, nous avons ici l'import du router et des proviers ainsi que le composant global toaster
  */
 const App = () => {
   return (
-    <ToasterProvider>
-      <FavoritesProvider>
-        <Toaster/>
-        <Routes />
-      </FavoritesProvider>
-    </ToasterProvider>
+    <></>
   );
 };
 
